e77f4244bbfdbade985cde6fd3fdd80655b58e3a
精细鬼 <2316336903@qq.com>
2023-01-14 18:22:08 +0800

    首页适配夜间模式主题

---
+++ js/dayNightSwitch.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+	var btn01 = document.getElementById("dayNightBtn");
+	var tips = "day";
+	var linkList = document.getElementsByTagName("link");
+	var imgs = document.getElementById("dayNightBtn").src;
+	btn01.onclick = function(){
+		if(tips == "day"){
+			tips = "night";
+			linkList[0].href = "./css/新拟态" + tips + ".css";
+		}else{
+			tips = "day";
+			linkList[0].href = "./css/新拟态" + tips + ".css";
+		}
+	}
+}
